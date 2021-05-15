175071a3822e768c5c0df0147c647af043b8b4c2
Isaac <isaacgeazy@gmail.com>
2021-05-15 12:40:56 -0300

    Sequencia de números pares

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace SequenciaPares
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             int contador = 0, n;
+
+                Console.Write("Números pares entre 0 e ? "); 
+                n = int.Parse(Console.ReadLine());
+
+            while (contador <= n){
+
+             Console.Write($"{ contador } ");
+              contador = contador + 2;
+            }
+        }
+    }
+}
